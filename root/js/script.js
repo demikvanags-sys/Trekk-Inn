0be3a634f4656d81549e062e387b44c52e0df1cd
Veronica Rosa <verosaperez1996@gmail.com>
2026-05-12 11:56:29 +0100

    nav bar and burguer menu

--- root/js/script.js
+++ root/js/script.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
 
     $('#welcome-screen').on('click', function() {
+        // 1. Deslizamos la pantalla de bienvenida hacia arriba
         $(this).addClass('slide-up');
         
-        // Esto hace que después de subir, la capa no moleste para tocar lo que haya debajo
+        // 2. Hacemos que el nav-bar aparezca suavemente (fade) 
+        // o aparezca de golpe justo cuando empieza la animación
+        $('#nav-bar').fadeIn(400); 
+
+        // 3. Opcional: Quitar el welcome del DOM después de que termine la animación
         setTimeout(function() {
-            $('#welcome-screen').css('display', 'none');
-        }, 700); 
+            $('#welcome-screen').hide();
+        }, 800);
+    });
+
+    // Evento del burger menu
+    $('#open-menu').on('click', function() {
+        $(this).toggleClass('active');
+        console.log("Menú abierto");
     });
 
 });
